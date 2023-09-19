3e6cc4e57a3bca3ccabe7e4865af56a5fdcb4743
Vinod <keshariyavinod793@gmail.com>
2023-09-19 12:02:10 +0530

    change styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-
+import React ,{ useEffect } from  "react";
+import Aos from "aos";
 const Footer = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, );
   return (
     <>
       <div className="footer">
         <div className="footer_container">
           <div className="footer_title">
-            <h2>Policies</h2>
+            <h2 data-aos="fade-right" data-aos-offset="200">Policies</h2>
           </div>
+
           <div className="policies">
             <a href="https://piimh.com/termsofservice/" target="_blank">
               <button>
