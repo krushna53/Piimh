cda2d2644ec0b36bde0a63188a5d0002d1b7ee71
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-09-21 10:54:56 +0530

    home page fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-// import Banner from "../Components/Banner";
+import Banner from "../Components/Banner";
 import BasicComponent from "../Components/BasicComponent";
 import Philosophy from "../Components/Philosophy";
-// import PhilosophyProgram from "../Components/PhilosophyProgram";
-// import Director from "../Components/Director";
-// import StayConnected from "../Components/StayConnected";
+import PhilosophyProgram from "../Components/PhilosophyProgram";
+import Director from "../Components/Director";
+import StayConnected from "../Components/StayConnected";
 const Home = () => {
   const id = "4hURH4J5WPqHSjm3vABwxo";
   console.log("Slug for Home Page:", id);
   return (
     <>
-      {/* <Banner slug={id} /> */}
+      <Banner slug={id} />
       <BasicComponent/>
       <Philosophy/>
-      {/* <PhilosophyProgram/> */}
-      {/* <Director/> */}
-      {/* <StayConnected/> */}
+      <PhilosophyProgram/>
+      <Director/>
+      <StayConnected/>
     </>
   );
 };
