4682d8057130f77430b12868b1bcccf85083f8b3
alka-kumari-hub <91412656+alka-kumari-hub@users.noreply.github.com>
2026-04-11 12:35:20 +0530

    Add getOrderStatus route to auth.js

--- Backend/src/api/routes/auth.js
+++ Backend/src/api/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createHdfcSession } = require("../controller");
+const { createHdfcSession, getOrderStatus } = require("../controller");
 const router = express.Router();
 
 router.post("/api/hdfc/create-session", createHdfcSession);
+router.get("/api/hdfc/order-status", getOrderStatus);
 
 
 module.exports.router = router;
