8501d85cce7b1a06e3ffed7c140f1af4f26bb197
sagarbharvadiya <sagarbharvadiya@gmail.com>
2026-06-17 12:05:03 +0530

    fix: remove hardcoded Firebase URIs causing secrets scan failure

--- netlify/functions/hdfc-payment-callback.js
+++ netlify/functions/hdfc-payment-callback.js
@@ -9,8 +9,6 @@ if (!admin.apps.length) {
       private_key: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI || "https://accounts.google.com/o/oauth2/auth",
-      token_uri: process.env.FIREBASE_TOKEN_URI || "https://oauth2.googleapis.com/token",
     };
     admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
   } catch (err) {
